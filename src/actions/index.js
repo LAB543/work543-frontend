95c999bdbe5be4e9c2fbd2859ef01d2be92fa7ea
Hwan Oh <hwangoon@gmail.com>
2018-08-10 17:00:29 +0900

    인증 통신파트 설정 및 테스트코드 입력

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,27 +2,36 @@
 import * as actions from '../constans/ActionTypes';
 import axios from 'axios';
 
+// for external api server connection - 201808101214 Hwan Oh
+const instance = axios.create({
+    baseURL: 'http://localhost:3000/v1',
+    timeout: 1000,
+    //headers: {'X-Custom-Header': 'foobar'}
+});
+
 export const userLogin = (data) => (dispatch) => {
-  const config = {'Content-Type': 'application/json'}
-  const authUser = axios.post('/api/auth/login', data, config);
+  const config = { 'Content-Type': 'application/json' };
+  const authUser = instance.post('/login', data, config);
 
   authUser.then(res => {
+    console.log(res);
     dispatch({
       type: actions.USER_LOGIN_SUCCESS,
       payload: res.data.token
     });
   })
   .catch(err => {
+    console.log(err);
     dispatch({
       type: actions.USER_LOGIN_FAIL,
       payload: err.response.data.message
     });
   });
-}
+};
 
 export const userRegister = (data) => (dispatch) => {
-  const config = { 'Content-Type': 'application/json' }
-  const authUser = axios.post('/api/auth/register', data, config);
+  const config = { 'Content-Type': 'application/json' };
+  const authUser = instance.post('/register', data, config);
 
   authUser.then(res => {
     dispatch({
@@ -36,4 +45,4 @@ export const userRegister = (data) => (dispatch) => {
         payload: err.response.data.message
       });
     });
-}
+};
